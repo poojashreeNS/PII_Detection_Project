50a914d26a3c12c64c505b49ac38e551bdc4525c
priyankamath16 <priyanka.math@sjsu.edu>
2023-05-05 20:44:41 -0700

    Changes to app.py

--- app.py
+++ app.py
@@ -3,57 +3,99 @@ from piidetection import OpenAI
 from piidetection import Piidetector
 import json
 import requests
+import numpy as np
+import faiss
+import openai
 
 app = Flask(__name__)
 
-model = OpenAI(api_key='OpenAI key ')
+openai_key = 'XXXX'
+ocrKey = 'XXXX'
+model = OpenAI(api_key = openai_key)
 nlp_prompter = Piidetector(model)
-ocrKey = 'OCR SPACE KEY'
+openai.api_key = openai_key
 
 @app.route('/', methods=["GET", "POST"])
 def detectPII():
     global result
     result = ''
     text_input = ''
-    active_tab = 'text'
+    active_tab = 'generic'
     if request.method == "POST":
-        examples = []
-        if len(request.form) > 0:
-
-            text_input = request.form['pii-text']
-            json_file = request.files['json-file']
-            if json_file and json_file.filename.endswith('.json'):
-                data = json.load(json_file)
-                for sample in data[:len(data)]:
-                    examples.append((sample['text'],sample['labels']))
-        else:
-            active_tab = 'file'
-            file_data = request.files['pii-file']
-            text_input = extractText(file_data)
-        output = nlp_prompter.fit('binary_classification.jinja',
+        if len(request.files) < 2: #check if tab1
+            active_tab = 'generic'
+            if(request.form['generic-text'] != ''): # if input is text
+                text_input = request.form['generic-text']
+            else: # if input is file
+                pii_files = request.files['generic-file']
+                text_input = extractText(pii_files)
+            output = nlp_prompter.fit('binary_classification.jinja',
                  label_0="pii",
                  label_1="non-pii",
-                 examples=examples,
                  text_input=text_input,
                  model_name="text-davinci-003")
-        result = eval(output['text'].strip())[0]['C'].lower()
+            result = eval(output['text'].strip())[0]['C'].lower()
+            return render_template('index.html', result=result, active_tab=active_tab)
+        else: #check if tab2
+            active_tab = 'custom'
+            custom_pii_files = request.files.getlist('custom-pii-file')
+            test_file = request.files.getlist('test-file')
+            
+            custom_input = extractText(custom_pii_files)
+            test_input = extractText(test_file)
+            
+            # Generate embeddings for the custom inputs
+            embeddings = np.array([get_embedding(text) for text in custom_input])
+
+            # Store the embeddings in a vector database
+            index = faiss.IndexFlatIP(embeddings.shape[1])
+            index.add(embeddings)
+
+            results = search(test_input, index, custom_input)
+            for result in results:
+                for item in result:
+                    if item[1] > 0.85:
+                        result='pii'
+                        break
+                    else:
+                        result='non-pii'
+                        continue
+                break
+            render_template('index.html', result=result, active_tab=active_tab)
     else:
         result = ''
     return render_template('index.html', result=result, active_tab=active_tab)
 
-def extractText(file_data):
+def extractText(files_data):
     payload = {'isOverlayRequired': False,
                'apikey': ocrKey,
                'language': 'eng',
                }
-    r = requests.post('https://api.ocr.space/parse/image',
+    text = []
+
+    for file_data in files_data:
+        r = requests.post('https://api.ocr.space/parse/image',
                 files={file_data.filename: file_data.read()},
                 data=payload,
             )
-    list = json.loads(r.content.decode())['ParsedResults']
-    text = ''
-    for sample in list:
-        text = text+sample['ParsedText']
-        print(sample['ParsedText'])
-        print('\n')
+        list = json.loads(r.content.decode())['ParsedResults']
+        file_texts = ''
+        for sample in list:
+            file_texts=file_texts+sample['ParsedText']
+        text.append(file_texts)
     return text
+
+# Define the function to generate embeddings
+def get_embedding(text, model="text-embedding-ada-002"):
+    text = text.replace("\n", " ")
+    return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
+
+# Define a function to perform search based on similarity score
+def search(queries, index, texts, k=1):
+    query_embeddings = [get_embedding(query) for query in queries]
+    results = []
+    for query_embedding in query_embeddings:
+        distances, indices = index.search(np.array([query_embedding]), k)
+        result = [(texts[i], distances[0][j]) for j, i in enumerate(indices[0])]
+        results.append(result)
+    return results
