e34a520368f695d537602adff600cc7936e27e60
priyankamath16 <priyanka.math@sjsu.edu>
2023-05-09 21:14:18 -0700

    feedback changes

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ import requests
 import numpy as np
 import faiss
 import openai
+import os
+import ast
 
 app = Flask(__name__)
 
-openai_key = 'XXXX'
-ocrKey = 'XXXX'
+openai_key = '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'
+ocrKey = 'K85726806988957'
 model = OpenAI(api_key = openai_key)
 nlp_prompter = Piidetector(model)
 openai.api_key = openai_key
+# Define the path to the index file
+index_path = "vector.index"
+custom_pii_path = "custom_pii.text"
+
 
 @app.route('/', methods=["GET", "POST"])
 def detectPII():
@@ -21,13 +27,18 @@ def detectPII():
     result = ''
     text_input = ''
     active_tab = 'generic'
+    test_input = ''
+    matched=''
+    score = 0
+    custom_texts = []
+    custom_error=False
     if request.method == "POST":
         if len(request.files) < 2: #check if tab1
             active_tab = 'generic'
             if(request.form['generic-text'] != ''): # if input is text
                 text_input = request.form['generic-text']
             else: # if input is file
-                pii_files = request.files['generic-file']
+                pii_files = request.files.getlist('generic-file')
                 text_input = extractText(pii_files)
             output = nlp_prompter.fit('binary_classification.jinja',
                  label_0="pii",
@@ -35,36 +46,82 @@ def detectPII():
                  text_input=text_input,
                  model_name="text-davinci-003")
             result = eval(output['text'].strip())[0]['C'].lower()
-            return render_template('index.html', result=result, active_tab=active_tab)
+            return render_template('index.html', custom_error=custom_error, result=result, test_input=test_input, matched=matched, score=score, active_tab=active_tab)
         else: #check if tab2
             active_tab = 'custom'
-            custom_pii_files = request.files.getlist('custom-pii-file')
+
+            if request.files.getlist('custom-pii-file')[0].filename != '':
+                custom_pii_files = request.files.getlist('custom-pii-file')
+                custom_input = extractText(custom_pii_files)
+                if os.path.isfile(custom_pii_path):
+                    with open(custom_pii_path, "r+") as f:
+                        tmp = f.read()
+                        custom_texts = ast.literal_eval(tmp)
+                        for text in custom_input:
+                            if text not in custom_texts:
+                                custom_texts.append(text)
+                        # custom_texts.extend(custom_input)
+                    with open(custom_pii_path, "w") as f:
+                        f.write(str(custom_texts))
+                else:
+                    with open(custom_pii_path, "w") as f:
+                        custom_texts.extend(custom_input)
+                        f.write(str(custom_texts))
+
+                if os.path.isfile(index_path):
+                    # Load the index from the file
+                    index = faiss.read_index(index_path)
+
+                # Generate embeddings for the custom inputs
+                embeddings = np.array([get_embedding(text) for text in custom_texts])
+
+                # Check if the index file exists
+                if os.path.isfile(index_path):
+                    # Load the index from the file
+                    index = faiss.read_index(index_path)
+                else:
+                    # Create a new index
+                    index = faiss.IndexFlatIP(embeddings.shape[1])
+
+                # # Check if each embedding is already present in the index before adding it
+                # for emb in embeddings:
+                #     distances, indices = index.search(np.array([emb]), k=1)
+                #     if distances[0][0] < 1:
+                #         print("Embedding already present in index")
+                #     else:
+                #         index.add(np.array([emb]))
+                # Add the embeddings to the index
+                index.add(embeddings)
+
+                # Save the index to the file
+                faiss.write_index(index, index_path)
+            else:
+                if os.path.isfile(custom_pii_path):
+                    with open(custom_pii_path, "r+") as f:
+                        tmp = f.read()
+                        custom_texts = ast.literal_eval(tmp)
+                else:
+                    return render_template('index.html', custom_error=True ,result=result, test_input=test_input, matched=matched, score=score, active_tab=active_tab)
             test_file = request.files.getlist('test-file')
-            
-            custom_input = extractText(custom_pii_files)
             test_input = extractText(test_file)
-            
-            # Generate embeddings for the custom inputs
-            embeddings = np.array([get_embedding(text) for text in custom_input])
 
-            # Store the embeddings in a vector database
-            index = faiss.IndexFlatIP(embeddings.shape[1])
-            index.add(embeddings)
-
-            results = search(test_input, index, custom_input)
+            results = search(test_input, custom_texts)
             for result in results:
                 for item in result:
                     if item[1] > 0.85:
                         result='pii'
+                        matched=item[0]
+                        score=item[1]
+                        test_input = test_input[0]
                         break
                     else:
                         result='non-pii'
                         continue
                 break
-            render_template('index.html', result=result, active_tab=active_tab)
+            return render_template('index.html', custom_error=custom_error, result=result, test_input=test_input, matched=matched, score=score, active_tab=active_tab)
     else:
         result = ''
-    return render_template('index.html', result=result, active_tab=active_tab)
+    return render_template('index.html', custom_error=custom_error, result=result, test_input=test_input, matched=matched, score=score, active_tab=active_tab)
 
 def extractText(files_data):
     payload = {'isOverlayRequired': False,
@@ -91,11 +148,14 @@ def get_embedding(text, model="text-embedding-ada-002"):
     return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
 
 # Define a function to perform search based on similarity score
-def search(queries, index, texts, k=1):
+def search(queries, texts, k=1):
+    # Retrieve the Faiss index from the .index file
+    index_retrieved = faiss.read_index(index_path)
+
     query_embeddings = [get_embedding(query) for query in queries]
     results = []
     for query_embedding in query_embeddings:
-        distances, indices = index.search(np.array([query_embedding]), k)
+        distances, indices = index_retrieved.search(np.array([query_embedding]), k)
         result = [(texts[i], distances[0][j]) for j, i in enumerate(indices[0])]
         results.append(result)
     return results
